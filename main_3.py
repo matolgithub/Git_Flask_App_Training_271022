f6a506c03129cbbd29e15f907386ccd74a32a039
matolgithub <89175955650@mail.ru>
2022-10-28 18:59:35 +0300

    Next commit to git.

--- main_3.py
+++ main_3.py
@@ -1,22 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+menu_list = ["menu_1", "menu_2", "menu_3"]
 
 
 @app.route("/")
 @app.route("/index/")
 def index_page():
-    return "It is index page!"
+    return render_template("index.html", text="Main page of the website!", main_string="--Flask - it is very COOL!--")
 
 
 @app.route("/about/")
 def about_page():
-    return "<h1>About us!</h1>"
+    return render_template("about.html")
 
+
+# @app.route("/list/")
+# def list_page():
+#     return [item for item in range(1, 11)]
 
 @app.route("/list/")
 def list_page():
-    return [item for item in range(1, 11)]
+    return render_template("list.html", title="List_List", string_1="gygygjyjgyjggjgyjgyg")
+
+
+@app.route("/menu/")
+def menu_page():
+    return render_template("menu.html", menu=menu_list)
 
 
 if __name__ == "__main__":
